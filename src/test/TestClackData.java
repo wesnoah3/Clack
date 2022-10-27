edd237c75bc132597790edb2bf60cf5b13f46d7f
Weston Hyde <weshyde3@gmail.com>
2022-10-27 15:02:18 -0400

    TestClasses WIP

--- src/test/TestClackData.java
+++ src/test/TestClackData.java
@@ -11,10 +11,11 @@ import java.util.Date;
 public class TestClackData {
     //This file should test all MessageClackdata and FileClackData methods
     public static void main(String[] args) {
-        // MessageClackData (both constructors)
+        // MessageClackData (all constructors)
         MessageClackData messageClackData1 = new MessageClackData();
         MessageClackData messageClackData2 =
                 new MessageClackData("testUser1", "testMessage", ClackData.CONSTANT_SENDMESSAGE);
+        MessageClackData messageClackData3 = new MessageClackData("testUser3", "HelloWorld", "SecretKey", 400);
 
         // FileClackData (both constructors)
         FileClackData fileClackData1 = new FileClackData();
@@ -24,6 +25,7 @@ public class TestClackData {
         // getType()
         System.out.println("messageClackData1 getType(): " + messageClackData1.getType());
         System.out.println("messageClackData2 getType(): " + messageClackData2.getType());
+        System.out.println("messageClackData3 getType(): " + messageClackData3.getType());
         System.out.println("fileClackData1 getType(): " + fileClackData1.getType());
         System.out.println("fileClackData2 getType(): " + fileClackData2.getType());
         System.out.println();
@@ -31,6 +33,7 @@ public class TestClackData {
         // getUserName()
         System.out.println("messageClackData1 getUserName(): " + messageClackData1.getUserName());
         System.out.println("messageClackData2 getUserName(): " + messageClackData2.getUserName());
+        System.out.println("messageClackData3 getUserName(): " + messageClackData3.getUserName());
         System.out.println("fileClackData1 getUserName(): " + fileClackData1.getUserName());
         System.out.println("fileClackData2 getUserName(): " + fileClackData2.getUserName());
         System.out.println();
@@ -38,6 +41,7 @@ public class TestClackData {
         // getDate()
         System.out.println("messageClackData1 getDate(): " + messageClackData1.getDate());
         System.out.println("messageClackData2 getDate(): " + messageClackData2.getDate());
+        System.out.println("messageClackData3 getDate(): " + messageClackData3.getDate());
         System.out.println("fileClackData1 getDate(): " + fileClackData1.getDate());
         System.out.println("fileClackData2 getDate(): " + fileClackData2.getDate());
         System.out.println();
@@ -45,6 +49,7 @@ public class TestClackData {
         // getData()
         System.out.println("messageClackData1 getData(): " + messageClackData1.getData());
         System.out.println("messageClackData2 getData(): " + messageClackData2.getData());
+        System.out.println("messageClackData3 getData(): " + messageClackData3.getData());
         System.out.println("fileClackData1 getData(): " + fileClackData1.getData());
         System.out.println("fileClackData2 getData(): " + fileClackData2.getData());
         System.out.println();
@@ -52,6 +57,7 @@ public class TestClackData {
         // hashCode()
         System.out.println("messageClackData1 hashCode(): " + messageClackData1.hashCode());
         System.out.println("messageClackData2 hashCode(): " + messageClackData2.hashCode());
+        System.out.println("messageClackData3 hashCode(): " + messageClackData3.hashCode());
         System.out.println("fileClackData1 hashCode(): " + fileClackData1.hashCode());
         System.out.println("fileClackData2 hashCode(): " + fileClackData2.hashCode());
         System.out.println();
@@ -65,6 +71,8 @@ public class TestClackData {
                 + messageClackData1.equals(messageClackData2));
         System.out.println("messageClackData2 equals messageClackData1: "
                 + messageClackData2.equals(messageClackData1));
+        System.out.println("messageClackData3 equals messageClackData1: "
+                + messageClackData3.equals(messageClackData1));
         System.out.println("messageClackData1 equals fileClackData1: "
                 + messageClackData1.equals(fileClackData1));
         System.out.println("fileClackData1 equals null: "
@@ -82,6 +90,7 @@ public class TestClackData {
         // toString()
         System.out.println("messageClackData1:\n" + messageClackData1);
         System.out.println("messageClackData2:\n" + messageClackData2);
+        System.out.println("messageClackData3:\n" + messageClackData3);
         System.out.println("fileClackData1:\n" + fileClackData1);
         System.out.println("fileClackData2:\n" + fileClackData2);
         System.out.println();
@@ -110,5 +119,8 @@ public class TestClackData {
         System.out.println("fileClackData2 equals fileClackData2: " + fileClackData2.equals(fileClackData2));
         System.out.println("fileClackData2 equals fileClackData1: " + fileClackData2.equals(fileClackData1));
         System.out.println("fileClackData2:\n" + fileClackData2);
+
+        // decrypt
+        System.out.println("messageClackData3 decrypted message: " + messageClackData3.getData("SecretKey"));
     }
 }
